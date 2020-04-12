72d82b0b0967d3ec672dce3fd597779b18bff3d5
nyknstyn <nicholas.gabriel048@gmail.com>
2020-04-12 15:00:12 +0530

    updated gitignore

---
+++ src/com/nyknstyn/code_pg/Main.java
@@ -0,0 +1,13 @@
+package com.nyknstyn.code_pg;
+
+/**
+ * Created by NG™
+ * Date: 12/04/20
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Design Pattern");
+    }
+}
